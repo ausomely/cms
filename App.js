bd0a978982a85883533457752344b74dbc08ebc1
ausomely <jaackieau@gmail.com>
2021-07-07 17:24:22 -0700

    view enginer handlebars

--- App.js
+++ App.js
@@ -2,27 +2,33 @@
 const express = require('express');
 const mongoose = require('mongoose'); 
 const path = require('path');
+const hdbs = require('express-handlebars');
+const { mongoDBUrl, PORT } = require('./config/configuration');
 
-const PORT = 3000;
 const app = express();
 
 /* Config Mongoose connection to MongoDB */
-mongoose.connect('mongodb://localhost:27017/cms', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(mongoDBUrl, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(res => {
         console.log('MongoDB Connected Successfully');
     }).catch(err => {
         console.log('Database connection failed');
     });
+
 /* Config express */
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
+/* Setup View Engine to Use Handlebars */
+app.engine('handlebars', hdbs( { defaultLayout: 'default' } ));
+app.set('view engine', 'handlebars');
+
 /* Routes */
 app.use('/', (req, res) => {
-    res.send('Welcome to the CMS App');
+    res.render('default/index');
 });
 
 app.listen(PORT, () => {
-    console.log('Server is running on port: ' + PORT);
+    console.log(`Server is running on port: ${PORT}`);
 });
